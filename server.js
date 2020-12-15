8c8f316d9c9a2c40830e9353b9c2b60cc87395c2
Rajeshroulo <rajraval017@gmail.com>
2020-12-15 12:28:47 +0530

    Added code to connect to mongo database

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const bodyParser= require('body-parser')
 const mongoose = require('mongoose')
 
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost/greetingApp', {
+  useUnifiedTopology: true
+}, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+})
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/greetings.html')
+})
+
+app.post('/sendgreeting', (req, res) => {
+  console.log(req.body)
 })
 
 app.listen(4000, () =>
